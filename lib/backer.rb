d3432b3972c776e295f99e5635fa8e81837f0fd7
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-07-19 17:10:16 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,11 @@
 class Backer 
   
-  attr_reader :backed_projects
+  attr_reader :backed_projects, :title 
   attr_accessor :name 
   
   def initialize(name)
     @name = name 
     @backed_projects = []
+    @title = title 
   end 
 end 
